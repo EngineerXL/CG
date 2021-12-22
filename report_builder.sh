c1012d3bc440a311a31321c122040614b6c112a2
EngineerXL <engineerxl@yahoo.com>
2021-12-22 21:20:17 +0300

    Исправил ошибку в скрипте

--- report_builder.sh
+++ report_builder.sh
@@ -3,7 +3,7 @@ for i in `ls -d */`; do
     cd $i
     if [[ -d ./report ]]; then
         cd ./report
-        make pdf
+        pdflatex report.tex
         cp report.pdf ../../reports/"report_${i:0:-1}.pdf"
         cd ..
     fi
